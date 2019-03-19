221f39399db01d5a8d392498c9e321347278d302
Jonathan Fessler <thefessler@gmail.com>
2019-03-18 20:27:19 -0700

    Bugfix in express GET Revs, and refactoring
    Move weighted-vote calculation code into separate file
    
    Also fix behavior in express GET Revs/{revId} to work properly for reviews with no votes

--- Routes/Restaurant/Revs.js
+++ Routes/Restaurant/Revs.js
@@ -2,41 +2,9 @@ var Express = require('express');
 var Tags = require('../Validator.js').Tags;
 var router = Express.Router({caseSensitive: true});
 var async = require('async');
+var Voting = require('./VoteWeighing.js');
 
 router.baseURL = '/Revs';
-
-var getBin = function(count, sum) {
-   var ramp;
-   var rawBin;
-
-   count = count || 0;
-   sum = sum || 0;
-
-   ramp = (count < 1 ? 1 : (count > 10 ? 10 : count)) / 10.0;
-
-   if (!count && !sum)
-      count = 1;
-
-   rawBin = (1.0*sum)/(1.0*count); // -1.0 <--> 1.0
-
-   rawBin = (2.5 * (rawBin + 1.0)); // 0.0 <--> 5.0
-
-   return (rawBin / 5.0) * ramp; // 0.0 <--> 1.0
-}
-
-var weighVote = function(voteObj) {
-   voteObj = voteObj || {};
-   var rawVote = (voteObj.voteValue || 0);
-   var vCnt = voteObj.count;
-   var vSum = voteObj.sum;
-
-   var bin = getBin(vCnt, vSum);
-   return rawVote * bin;
-};
-
-var voteReducer = function(acc, cur) {
-   return weighVote(cur) + acc;
-}
 
 router.get('/:revId', function(req, res) {
    var vld = req.validator;
@@ -47,13 +15,15 @@ router.get('/:revId', function(req, res) {
    + ' where r.id = ?';
 
    // http://sqlfiddle.com/#!9/a78bff/48
-   var votsFromRevQry = 'SELECT outerv.id, outerv.rstId, outerv.prsId, outerv.voteValue, pcol.count, pcol.sum '
+   var votsFromRevQry = 'SELECT outerv.id, outerv.rstId, outerv.prsId, '
+   + 'outerv.voteValue, pcol.count, pcol.sum '
    + 'FROM Vote outerv '
    + 'JOIN (SELECT p.id, SUM(rcol.count) as count, SUM(rcol.sum) as sum '
       + 'FROM Person p '
       + 'LEFT JOIN (SELECT r.id, r.prsId, updown.count, updown.sum '
          + 'FROM Review r '
-         + 'JOIN (SELECT v.revId, COUNT(v.id) AS count, SUM(v.voteValue) AS sum '
+         + 'JOIN (SELECT v.revId, COUNT(v.id) AS count, SUM(v.voteValue) '
+            + 'AS sum '
             + 'FROM Vote v '
             + 'GROUP BY v.revId) updown '
          + 'ON updown.revId = r.id) rcol '
@@ -76,27 +46,26 @@ router.get('/:revId', function(req, res) {
       }
    },
    function(vots, fields, cb) {
-      if (vld.check(vots.length, Tags.notFound, null, cb)) {
-         // id     rstId     prsId     voteValue     count     sum
-         var weightedVotes = vots.reduce(voteReducer, 1.0);
-         var revObj = {
-            rstId: revResult.rstId,
-            whenMade: revResult.rstId ? revResult.rstId.getTime() : 0,
-            email: revResult.email,
-            content: revResult.content,
-            rating: revResult.rating,
-            numUpvotes: Math.floor(weightedVotes),
-            ownerResponse: (revResult.ownerResponseWhenMade
-             && revResult.ownerResponseContent)
-             ? {
-               whenMade: revResult.ownerResponseWhenMade,
-               content: revResult.ownerResponseContent
-             }
-             : null
-         };
-         res.json(revObj);
-         cb();
-      }
+      // id     rstId     prsId     voteValue     count     sum
+      var weightedVotes = vots.reduce(Voting.WeightedVoteReducer, 1.0);
+      var revObj = {
+         rstId: revResult.rstId,
+         whenMade: revResult.rstId ? revResult.rstId.getTime() : 0,
+         email: revResult.email,
+         title: revResult.title,
+         content: revResult.content,
+         rating: revResult.rating,
+         numUpvotes: Math.floor(weightedVotes),
+         ownerResponse: (revResult.ownerResponseWhenMade
+          && revResult.ownerResponseContent)
+          ? {
+          whenMade: revResult.ownerResponseWhenMade,
+          content: revResult.ownerResponseContent
+          }
+          : null
+      };
+      res.json(revObj);
+      cb();
    }],
 
    function(err) {
